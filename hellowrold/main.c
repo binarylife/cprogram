5786ae6153103ff1ba956df6e6fc9bb3a4bc06cb
fengbei <fengbei@chehejia.com>
2020-03-31 15:28:42 +0800

    init project

---
+++ hellowrold/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  hellowrold
+//
+//  Created by bei feng on 2020/3/7.
+//  Copyright © 2020 bei feng. All rights reserved.
+//
+
+#include <stdio.h>
+#include <string.h>
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, 冯贝 World!\n");
+    
+    
+    printf("\n\n 整数类型 \n\n ");
+    
+    printf("char 存储大小: %lu \n",sizeof(char));
+
+    return 0;
+}
